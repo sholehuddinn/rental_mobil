7c931341d02ff738a753250486fe75a27612a964
Fajar <fajaruddin.soleh@gmail.com>
2025-02-12 01:22:33 +0700

    mboh

--- src/pages/CreteOrder.jsx
+++ src/pages/CreteOrder.jsx
@@ -50,7 +50,7 @@ const CreteOrder = () => {
             to={`/order/create/${car.id}`}
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
-              Lihat Detail
+              Pesan
             </Link>
           </div>
         );
